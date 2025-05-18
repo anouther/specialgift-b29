6a2387d98f17e952ccb7f323f9e99ca1bdc1c80f
anouther <155570107+anouther@users.noreply.github.com>
2025-05-18 18:11:58 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
       const c = setTimeout(() => {
         document.body.classList.remove("not-loaded");
 
-        const titles = ('isaac i love you').split('');
+        const titles = ('happy birthday!').split('');
         const titleElement = document.getElementById('title');
         let index = 0;
 
@@ -21,7 +21,7 @@
           } else {
             // Setelah teks utama selesai, tambahkan subtitle kecil
             const subtitle = document.createElement('p');
-            subtitle.innerText = "terimakasi udaaah kasi gift yang spesial juga buat aku, dan terimakasi sudah ngehargain semua gift dari aku";
+            subtitle.innerText = "you make the world a brighter place just by being in it.)";
             subtitle.style.fontSize = '20px'; // Ukuran font lebih kecil
             subtitle.style.marginTop = '2px'; // Spasi antara teks utama dan subtitle
             subtitle.style.color = '#fff'; // Warna abu-abu untuk teks subtitle
